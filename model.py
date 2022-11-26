cce408eb783b4cdc5d1a9c19f16c3b58ebe2ebcb
Eric Tang <tang8156@gmail.com>
2022-11-26 01:03:09 -0500

    works

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-
+import torch.nn.functional as F
 
 class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
@@ -23,11 +23,30 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
-    
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.embeddings = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
+
     def forward(self, features, captions):
-        pass
+        embeddings = self.embeddings(captions[:,:-1])
+        x = torch.cat((features.unsqueeze(1), embeddings), dim = 1)
+        x, _ = self.lstm(x)
+        x = self.fc(x)
+        return x
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        caption = []
+        hidden = (torch.randn(self.num_layers, 1, self.hidden_size).to(inputs.device),
+                  torch.randn(self.num_layers, 1, self.hidden_size).to(inputs.device))
+        for i in range(max_len):
+            x, hidden = self.lstm(inputs, hidden)
+            x = self.fc(x)
+            x = x.squeeze(1)
+            wordid = x.argmax(dim=1)
+            caption.append(wordid.item())
+            inputs = self.embeddings(wordid.unsqueeze(0))
+        return caption
